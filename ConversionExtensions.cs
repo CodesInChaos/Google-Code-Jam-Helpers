c371938764f49b97bd6e780bf41b5c38e617d1a9
CodeInChaos <CodeInChaos@nowhere>
2011-06-12 16:01:12 +0200

    Initial - What I used for Code Jam 2011

---
+++ ConversionExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Numerics;
+
+namespace JamHelpers
+{
+	public static class ConversionExtensions
+	{
+		public static string[] Split(this string s)
+		{
+			return s.Split(' ');
+		}
+
+		public static string Join<T>(this IEnumerable<T> parts)
+		{
+			return string.Join(" ", parts.Select(o => o.ToStringInv()));
+		}
+
+		//Scalar
+
+		public static int ToInt(this string s)
+		{
+			return int.Parse(s, CultureInfo.InvariantCulture);
+		}
+
+		public static BigInteger ToBigInt(this string s)
+		{
+			return BigInteger.Parse(s, CultureInfo.InvariantCulture);
+		}
+
+		public static Int64 ToInt64(this string s)
+		{
+			return Int64.Parse(s, CultureInfo.InvariantCulture);
+		}
+
+		public static double ToDouble(this string s)
+		{
+			return double.Parse(s, CultureInfo.InvariantCulture);
+		}
+
+		public static string ToStringInv(this object o)
+		{
+			var formattable = o as IFormattable;
+			if (formattable != null)
+				return formattable.ToString(null, CultureInfo.InvariantCulture);
+			else
+				return o.ToString();
+		}
+
+		//array
+		public static int[] ToInt(this string[] s)
+		{
+			return s.Select(ToInt).ToArray();
+		}
+
+		public static BigInteger[] ToBigInt(this string[] s)
+		{
+			return s.Select(ToBigInt).ToArray();
+		}
+
+		public static Int64[] ToInt64(this string[] s)
+		{
+			return s.Select(ToInt64).ToArray();
+		}
+
+		public static double[] ToDouble(this string[] s)
+		{
+			return s.Select(ToDouble).ToArray();
+		}
+
+		public static string[] ToStringInv<T>(this T[] arr)
+		{
+			return arr.Select(o => o.ToStringInv()).ToArray();
+		}
+	}
+}
---
+++ ProgramBase.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace JamHelpers
+{
+	public abstract class ProgramBase
+	{
+		public TextReader Input;
+		public TextWriter Output;
+
+		public int CaseNo;
+		public int CaseCount;
+
+		public string ReadLine()
+		{
+			return Input.ReadLine();
+		}
+
+		public string[] ReadMultiLine()
+		{
+			int count = ReadLine().ToInt();
+			string[] result = new string[count];
+			for (int i = 0; i < count; i++)
+				result[i] = ReadLine();
+			return result;
+		}
+
+		public void WriteLine(object s)
+		{
+			Output.WriteLine(s.ToStringInv());
+			Console.WriteLine(s.ToStringInv());
+		}
+
+		public void Write(object s)
+		{
+			Output.Write(s.ToStringInv());
+			Console.Write(s.ToStringInv());
+		}
+
+		public void DebugWrite(object s)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Write(s.ToStringInv());
+			Console.ResetColor();
+		}
+
+		public void DebugWriteLine(object s)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine(s.ToStringInv());
+			Console.ResetColor();
+		}
+
+		public string CaseStr { get { return "Case #" + CaseNo + ":"; } }
+
+		public abstract void RunCase();
+
+		public virtual void Run()
+		{
+			string inputFile;
+			string dir = "../../";
+			if (File.Exists(dir+"input.in"))
+				inputFile = "input.in";
+			else inputFile = "input.txt";
+			Console.ForegroundColor = ConsoleColor.Red;
+			if (inputFile == "input.txt")
+				Console.WriteLine("Practice");
+			else
+				Console.WriteLine("Real");
+			Console.WriteLine();
+			using (Input = new StreamReader(dir + inputFile))
+			using (Output = new StreamWriter(dir + "output.txt"))
+			{
+				CaseCount = Input.ReadLine().ToInt();
+				for (CaseNo = 1; CaseNo <= CaseCount; CaseNo++)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("Case " + CaseNo + "/" + CaseCount);
+					Console.ResetColor();
+					RunCase();
+				}
+			}
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Finished");
+			Console.ResetColor();
+			Console.ReadKey();
+		}
+	}
+}
