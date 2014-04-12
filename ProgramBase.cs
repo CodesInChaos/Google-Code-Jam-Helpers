10346ebc89a3d06a17c1d0d996650b6926627f5b
CodesInChaos <CodesInChaos@gmail.com>
2014-04-12 18:58:27 +0200

    Work with all .in files WriteAnswer helper

--- ProgramBase.cs
+++ ProgramBase.cs
@@ -6,87 +6,100 @@ using System.IO;
 
 namespace JamHelpers
 {
-	public abstract class ProgramBase
-	{
-		public TextReader Input;
-		public TextWriter Output;
+    public abstract class ProgramBase
+    {
+        public TextReader Input;
+        public TextWriter Output;
 
-		public int CaseNo;
-		public int CaseCount;
+        public int CaseNo;
+        public int CaseCount;
 
-		public string ReadLine()
-		{
-			return Input.ReadLine();
-		}
+        public string ReadLine()
+        {
+            return Input.ReadLine();
+        }
 
-		public string[] ReadMultiLine()
-		{
-			int count = ReadLine().ToInt();
-			string[] result = new string[count];
-			for (int i = 0; i < count; i++)
-				result[i] = ReadLine();
-			return result;
-		}
+        public string[] ReadMultiLine()
+        {
+            int count = ReadLine().ToInt();
+            string[] result = new string[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadLine();
+            return result;
+        }
 
-		public void WriteLine(object s)
-		{
-			Output.WriteLine(s.ToStringInv());
-			Console.WriteLine(s.ToStringInv());
-		}
+        public void WriteLine(object s)
+        {
+            Output.WriteLine(s.ToStringInv());
+            Console.WriteLine(s.ToStringInv());
+        }
 
-		public void Write(object s)
-		{
-			Output.Write(s.ToStringInv());
-			Console.Write(s.ToStringInv());
-		}
+        public void Write(object s)
+        {
+            Output.Write(s.ToStringInv());
+            Console.Write(s.ToStringInv());
+        }
 
-		public void DebugWrite(object s)
-		{
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.Write(s.ToStringInv());
-			Console.ResetColor();
-		}
+        public void DebugWrite(object s)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(s.ToStringInv());
+            Console.ResetColor();
+        }
 
-		public void DebugWriteLine(object s)
-		{
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine(s.ToStringInv());
-			Console.ResetColor();
-		}
+        public void DebugWriteLine(object s)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(s.ToStringInv());
+            Console.ResetColor();
+        }
 
-		public string CaseStr { get { return "Case #" + CaseNo + ":"; } }
+        public void WriteAnswer(object o)
+        {
+            WriteLine(CaseStr + " " + o.ToStringInv());
+        }
 
-		public abstract void RunCase();
+        public string CaseStr { get { return "Case #" + CaseNo + ":"; } }
 
-		public virtual void Run()
-		{
-			string inputFile;
-			string dir = "../../";
-			if (File.Exists(dir+"input.in"))
-				inputFile = "input.in";
-			else inputFile = "input.txt";
-			Console.ForegroundColor = ConsoleColor.Red;
-			if (inputFile == "input.txt")
-				Console.WriteLine("Practice");
-			else
-				Console.WriteLine("Real");
-			Console.WriteLine();
-			using (Input = new StreamReader(dir + inputFile))
-			using (Output = new StreamWriter(dir + "output.txt"))
-			{
-				CaseCount = Input.ReadLine().ToInt();
-				for (CaseNo = 1; CaseNo <= CaseCount; CaseNo++)
-				{
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine("Case " + CaseNo + "/" + CaseCount);
-					Console.ResetColor();
-					RunCase();
-				}
-			}
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.WriteLine("Finished");
-			Console.ResetColor();
-			Console.ReadKey();
-		}
-	}
+        public abstract void RunCase();
+
+        public virtual void RunFile()
+        {
+            CaseCount = Input.ReadLine().ToInt();
+            for (CaseNo = 1; CaseNo <= CaseCount; CaseNo++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Case " + CaseNo + "/" + CaseCount);
+                Console.ResetColor();
+                RunCase();
+            }
+        }
+
+        public virtual void Run()
+        {
+            const string dir = "../../";
+            var files = Directory.EnumerateFiles(dir, "input.txt").Concat(Directory.EnumerateFiles(dir, "*.in")).ToArray();
+            foreach (var inputFile in files)
+            {
+                var outputFile = Path.ChangeExtension(inputFile, ".out");
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (inputFile == "input.txt")
+                    Console.WriteLine("Practice");
+                else
+                    Console.WriteLine("Real - " + Path.GetFileName(inputFile));
+                Console.WriteLine();
+                using (Input = new StreamReader(inputFile))
+                using (Output = new StreamWriter(outputFile))
+                {
+                    RunFile();
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Finished");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
 }
